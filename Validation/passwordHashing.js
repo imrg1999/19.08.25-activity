008a089fcf0a62c9f80718ef69918b95144956e9
rupankar1999 <rupankar.ghosh1999@gmail.com>
2025-08-20 10:17:07 +0530

    hashing export mode updated, due to an error

--- Validation/passwordHashing.js
+++ Validation/passwordHashing.js
@@ -2,6 +2,8 @@ import bcrypt from 'bcrypt';
 
 const saltRounds = 10;
 
-export const hashing = async(password) => {
+const hashing = async(password) => {
     return await bcrypt.hash(password, saltRounds);
 }
+
+export default hashing;
