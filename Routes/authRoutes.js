8be60f57786c945be61847003c819ec28158a8a6
rupankar1999 <rupankar.ghosh1999@gmail.com>
2025-08-23 20:40:22 +0530

    profile access path added to authRoutes.js

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { registerUser, loginUser } from "../Controller/authController.js";
+import { registerUser, loginUser, userProfile } from "../Controller/authController.js";
+import { authMiddleware } from '../Middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
+router.post('/profile',authMiddleware,userProfile);
 
 
 export default router;
